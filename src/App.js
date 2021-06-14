31a3f17f55efdd22c3a97cf3cbf0930113f42065
ahilal697 <ahilal697@gmail.com>
2021-06-14 23:45:48 +0530

    adding header component to our app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import './App.css';
-import { Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 
-import HomePage from './pages/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component.jsx'
+import Header from './components/header/header.component.jsx'
 
-
-import ShopPage from './pages/shoppage/shoppage.component';
+import ShopPage from './pages/shoppage/shoppage.component.jsx';
 
 
 
 function App() {
   return (
     <div>
+      <Header/>
+      <Switch>
       <Route exact path='/' component={HomePage}/>
       <Route exact path='/shop' component={ShopPage}/>
+      </Switch>
     </div>
   );
 }
